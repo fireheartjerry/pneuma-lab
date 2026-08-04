92a8246bc732a8d424de054ccb8dc5ef53927bcb
Jerry <fireheartjerry@gmail.com>
2026-08-03 20:48:21 -0700

    scope qualification image binding to worker runtime

--- src/pneuma_lab/cloud/images.py
+++ src/pneuma_lab/cloud/images.py
@@ -22,6 +22,8 @@ _QUALIFICATION_IMAGE_RUNTIME_PATHS = (
     "src/pneuma_lab/**",
     ":!src/pneuma_lab/cloud/ephemeral_runner.py",
     ":!src/pneuma_lab/cloud/images.py",
+    ":!src/pneuma_lab/cloud/ephemeral_receipt.py",
+    ":!schemas/cloud-ephemeral-dual-worker-qualification-receipt.schema.json",
 )
 
 
